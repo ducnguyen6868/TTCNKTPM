f6b74824a36af23f92259d9c5d66655aed3a5af3
ducnguyen6868 <duc460169@gmail.com>
2024-03-22 23:18:46 +0700

    second commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 const express= require("express");
 const port= 3000;
+const path= require("path");
+const handlebars=require( "express-handlebars");
+const bodyParser = require('body-parser');
 const app =  express();
+app.use(express.static(path.join(__dirname, 'public')))
+console.log(path.join(__dirname));
+app.use(express.urlencoded({
+  extended: true
+}))
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.engine('hbs', handlebars.engine({
+  extname: '.hbs'
+}))
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, 'resources/views'))
 app.get("/",(req,res)=>{
-    res.send("<h1>Hello World</h1>")
+    res.render("homepage")
     })
 app.listen(port,()=>{console.log(`Server is running on ${port}`)})
