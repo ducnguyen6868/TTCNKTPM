bbd9939786d0c3aa0c79895720481b2548a23512
ducnguyen6868 <duc460169@gmail.com>
2024-03-28 20:18:11 +0700

    homepage interface

--- src/public/js/hompage.js
+++ src/public/js/hompage.js
@@ -19,15 +19,39 @@ setInterval(() => {
     }
 }, 3000);
 
-var options = document.querySelectorAll('.option');
 
-options.forEach(function(option) {
-    option.addEventListener('click', function() {
-        // Loại bỏ lớp "selected" từ tất cả các option trước đó
-        options.forEach(function(opt) {
-            opt.classList.remove('selected');
-        });
-        // Thêm lớp "selected" vào option được nhấp
-        this.classList.add('selected');
-    });
+
+//Ẩn hiện menu theo từng mục 
+const combo = document.getElementById("combo-button");
+const combo_list = document.getElementById("combo");
+const pizza = document.getElementById("pizza-button");
+const pizza_list = document.getElementById("pizza");
+const appetizer = document.getElementById("appetizer-button");
+const appetizer_list = document.getElementById("appetizer");
+combo.addEventListener('click', () => {
+    combo_list.style.display = 'grid';
+    combo.classList.add('border-bottom');
+    appetizer_list.style.display='none';
+    appetizer.classList.remove('border-bottom');
+    pizza_list.style.display = 'none';
+    pizza.classList.remove('border-bottom');
 });
+
+pizza.addEventListener('click', () => {
+    combo_list.style.display = 'none';
+    combo.classList.remove('border-bottom');
+    appetizer_list.style.display = 'none';
+    appetizer.classList.remove('border-bottom');
+    pizza_list.style.display = 'grid';
+    pizza.classList.add('border-bottom');
+
+});
+appetizer.addEventListener('click', () => {
+    combo_list.style.display = 'none';
+    combo.classList.remove('border-bottom');
+    pizza_list.style.display = 'none';
+    pizza.classList.remove('border-bottom');
+    appetizer_list.style.display = 'grid';
+    appetizer.classList.add('border-bottom');
+});
+
